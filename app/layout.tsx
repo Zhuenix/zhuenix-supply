abcc543ca943890e06e17b2d1b29ebf97ceb6fb4
Zhuenix <337315382@qq.com>
2026-05-25 11:36:31 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import "./globals.css";
+import './globals.css'
 
 export const metadata = {
-  title: "Zhuenix Supply",
-  description: "Global Supply Chain Partner",
-};
+  title: 'Zhuenix Supply',
+  description: 'Global Supply Chain Partner',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
